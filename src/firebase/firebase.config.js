bd876f15d318aac3b38dc89b6330644396c28add
Dibbo T <dibbotripura7@gmail.com>
2023-10-03 11:05:46 +0600

    react firebase created

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbVILXLx211d3-mepH1kIgVZwkW-a-6-0",
+  authDomain: "react-firebase-news-20f71.firebaseapp.com",
+  projectId: "react-firebase-news-20f71",
+  storageBucket: "react-firebase-news-20f71.appspot.com",
+  messagingSenderId: "285585819363",
+  appId: "1:285585819363:web:11367bdf431e27ae7967d6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
