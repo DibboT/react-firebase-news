db6b40443ab855d4ef156297314f11d4fd45d4c7
Dibbo T <dibbotripura7@gmail.com>
2023-10-03 13:43:44 +0600

    right side navbar created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import Header from "./Shared/Header";
+import NavBar from "./Shared/NavBar";
 
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <NavBar></NavBar>
             <h2 className="text-3xl font-poppins font-semibold">This is home</h2>
         </div>
     );
---
+++ src/pages/Shared/RightSideNav.jsx
@@ -0,0 +1,11 @@
+
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2 className="">Right Side Nav</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
