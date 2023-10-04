02a6e347a7e7acbb8594a8eef6f07ec37b8cf3fa
Dibbo T <dibbotripura7@gmail.com>
2023-10-04 11:25:46 +0600

    moment js added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Header from "./Shared/Header";
+import LeftSideNav from "./Shared/LeftSideNav";
 import NavBar from "./Shared/NavBar";
+import RightSideNav from "./Shared/RightSideNav";
 
 
 const Home = () => {
@@ -8,6 +10,18 @@ const Home = () => {
             <Header></Header>
             <NavBar></NavBar>
             <h2 className="text-3xl font-poppins font-semibold">This is home</h2>
+
+            <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
+                <div className="border">
+                    <LeftSideNav></LeftSideNav>
+                </div>
+                <div className="col-span-2 border">
+                    <h3 className="text-2xl">News is Coming Soon</h3>
+                </div>
+                <div className="border">
+                    <RightSideNav></RightSideNav>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Shared/Header.jsx
+++ src/pages/Shared/Header.jsx
@@ -1,9 +1,12 @@
-
+import moment from 'moment';
+import logo from '../../assets/logo.png'
 
 const Header = () => {
     return (
         <div>
-            
+            <img src={logo} alt=""/>
+            <p>Journalism Without Fear or Favour</p>
+            <p>{moment().format("dddd, MMMM D YYYY, h:mm:ss a")}</p>
         </div>
     );
 };
